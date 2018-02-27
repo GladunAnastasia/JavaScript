1be8f104c0136d7a32f78c53ef282a50feef101a
Gladun Anastasia <Anastasia.Gladun@aton.ru>
2018-02-27 10:11:44 +0300

    BinToDec.js

---
+++ BinToDec.js
@@ -0,0 +1,18 @@
+function BinToDec(number) {
+	number = String(number);
+	if(!rightNumber(number)) {
+		return;
+	}
+	let result = 0;
+	numberArray = number.split('');
+	numberArray.forEach(function(value, index) {result += value * Math.pow(2,Math.abs(index-(number.length-1)))});
+	return result;
+}
+function rightNumber(number) {
+	for(let i = 0; i < number.length; i++) {
+		if(number[i] != '0' && number[i] != '1') {
+			return false;
+		}
+	}
+	return true;
+}
