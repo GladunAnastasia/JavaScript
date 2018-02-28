7a1ad2599d48554e8170710768925ae13d99affb
Gladun Anastasia <Anastasia.Gladun@aton.ru>
2018-02-28 17:55:49 +0300

    Task2/linkedList.js

--- Task2/linkedList.js
+++ Task2/linkedList.js
@@ -1,10 +1,9 @@
-function item(value) {
+function Item(value) {
 	this.value = value;
 }
-
-linkedList = {
-	add(element) {
-		let it = new item(element);
+function LinkedList(number) {
+	this.add = function(element) {
+		let it = new Item(element);
 		if(this.last === undefined) {
 			this.first = it;
 			this.last = it;
@@ -13,17 +12,17 @@ linkedList = {
 			this.last.next.prev = this.last;
 			this.last = this.last.next;
 		}
-	},
-	get(element) {
+	}
+	this.get = function(element) {
 		let tempItem = this.first;
 		while(tempItem !== undefined) {
-			if(tempItem.value === element) {
+			if(tempItem.value == element) {
 				return tempItem;
 			}
 			tempItem = tempItem.next;
 		}
-	},
-	removeFirst(element) {
+	}
+	this.removeFirst = function(element) {
 		let tempItem = this.get(element);
 		if (tempItem !== undefined){
 			if(this.first === tempItem && this.last === tempItem){ 
@@ -40,8 +39,8 @@ linkedList = {
 				tempItem.next.prev = tempItem.prev;
 			}
 		}
-	},
-	removeAll(element) {
+	}
+	this.removeAll = function(element) {
 		let tempItem;
 		while((tempItem = this.get(element))){
 			if(this.first === tempItem && this.last === tempItem){ 
@@ -58,32 +57,64 @@ linkedList = {
 				tempItem.next.prev = tempItem.prev;
 			}
 		}
-	},
-	printList() {
+	}
+	this.clear = function() {
+		this.first = undefined;
+		this.last = undefined;
+	}
+	this.printList = function() {
 		let tempItem = this.first;
+		console.log(tempItem ? "Список: " : "Список пуст");
 		while(tempItem !== undefined) {
 			console.log(tempItem);
 			tempItem = tempItem.next;
 		}
 	}
+	this.plus = function(list) {
+		let tempItem = this.first;
+		let thisSum = getSum(tempItem);
+		tempItem = list.first;
+		let listSum = getSum(tempItem);
+		let result = Number(thisSum) + Number(listSum);
+		function getSum(item) {
+			let sum = 0;
+			while(item !== undefined) {
+				sum += item.value;
+				item = item.next;
+			}
+			return sum;
+		}
+		return new LinkedList(String(result).split('').reverse().join(''));
+	}
+	this.rightNumber = function(number) {
+		let flag = true;
+		String(number).split('').forEach(function(value) {if(value === 'e') {flag = false}});
+		return flag;
+	}
+	this.numberToList = function(number) {
+		console.log("HERE!");
+		if(isNaN(number)) {
+			console.log("В JavaScript нет представления для введенного числа. Создан пустой список.");
+			return new LinkedList();
+		}
+		if(typeof number === 'number' && number > Number.MAX_SAFE_INTEGER || !this.rightNumber(number)) {
+			console.log("Слишком длинное число или число с экспонентой. Передайте число в формате строки. Создан пустой список.");
+			return new LinkedList();
+		}
+		let list = new LinkedList();
+		String(number).split('').forEach(function(value, index) {list.add(value)});
+		console.log(list);
+		return list;
+	}
+	if(number !== undefined) {
+		return this.numberToList(number);
+	}
 }
-linkedList.add(1);
-linkedList.add(2);
-linkedList.add(3);
-linkedList.add(4);
-linkedList.add(5);
-linkedList.printList();
-console.log('-----------------------------');
-linkedList.add(1);
-linkedList.add(2);
-linkedList.add(3);
-linkedList.add(4);
-linkedList.add(5);
-linkedList.removeAll(7);
-linkedList.removeAll(1);
-/*linkedList.removeFirst(1);
-linkedList.removeFirst(2);
-linkedList.removeFirst(3);
-linkedList.removeFirst(4);
-linkedList.removeFirst(5);*/
-linkedList.printList();
+let listFirst = new LinkedList('243');
+let listSecond = new LinkedList('564');
+listFirst.plus(listSecond).printList();//--->  '708'
+let list = (new LinkedList('12345678946573645762398489899'));
+list.removeAll(1);
+list.removeAll(9);
+list.removeAll(8);
+list.printList();
