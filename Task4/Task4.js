4850a6dd3512230640c7c566d88623325362a5e3
GladunAnastasia <netmislei@gmail.com>
2018-03-20 21:19:35 +0300

    Task4/Task4.js

--- Task4/Task4.js
+++ Task4/Task4.js
@@ -14,7 +14,6 @@ class Node{
                 this.children.push(children);
             }
         }
-        this.mark = false;
     }
 }
 class Graph{
@@ -45,6 +44,12 @@ class Graph{
             firstNode.addChildren(secondNode);
             secondNode.addChildren(firstNode);
         }
+        this.clearParams = function() {
+        	for (let node of this.nodes) {
+	        	node.mark = false;
+	        	node.parent = undefined;
+	        }
+	    }
     }
 
     depthFirstSearch() {
@@ -67,6 +72,7 @@ class Graph{
                 node = node.parent;
             }
         }while(node.parent || !node.children.every(function(x){return x.mark}));
+        this.clearParams();
         return result;
     }
 
@@ -90,10 +96,11 @@ class Graph{
             }
             array = arrayTemp;
         }while(arrayTemp.length != 0);
+        this.clearParams();
         return result;
     }
 
-    theShortestDistence(first, second) {
+    theShortestDistance(first, second) {
         let firstNode = this.getNode(first);
         let secondNode = this.getNode(second);
         firstNode.mark = true;
@@ -116,29 +123,35 @@ class Graph{
                 } 
             }
             firstNode.mark = true;
-            if(!firstNode.children.every(function(x){return x.mark})) {
+            if(minNode) {
                 firstNode = minNode;
             } else {
                 firstNode = firstNode.parent;
             }
         }
+        this.clearParams();
         return secondNode.min;
     }
 }
-console.log((new Graph([[1,0],[0,5],[0,4],[1,2],[1,3],[3,2],[2,4],[4,5],[5,6],[2,6],[6,9],[2,9],[5,7],[5,8],[8,11],[7,11],[7,10],[10,11],[9,7]])).depthFirstSearch());
-console.log((new Graph([[0,6],[2,4],[7,1],[2,1],[5,3],[1,3],[0,3]])).depthFirstSearch());
-console.log((new Graph([[0,3],[1,3],[2,3],[4,3],[5,4]])).depthFirstSearch());
-console.log((new Graph([[1,0],[0,5],[0,4],[1,2],[1,3],[3,2],[2,4],[4,5],[5,6],[2,6],[6,9],[2,9],[5,7],[5,8],[8,11],[7,11],[7,10],[10,11],[9,7]])).breadthFirstSearch());
-console.log((new Graph([[0,6],[2,4],[7,1],[2,1],[5,3],[1,3],[0,3]])).breadthFirstSearch());
-console.log((new Graph([[0,3],[1,3],[2,3],[4,3],[5,4]])).breadthFirstSearch());
+let graph1 = new Graph([[1,0],[0,5],[0,4],[1,2],[1,3],[3,2],[2,4],[4,5],[5,6],[2,6],[6,9],[2,9],[5,7],[5,8],[8,11],[7,11],[7,10],[10,11],[9,7]]);
+let graph2 = new Graph([[0,6],[2,4],[7,1],[2,1],[5,3],[1,3],[0,3]]);
+let graph3 = new Graph([[0,3],[1,3],[2,3],[4,3],[5,4]]);
+let graph4 = new Graph([[1,6,14],[1,2,7],[1,3,9],[2,4,15],[2,3,10],[3,6,2],[3,4,11],[6,5,9],[5,4,6]]);
+let graph5 = new Graph([[0,3,5],[1,3,11],[2,3,56],[4,3,77],[5,4,89]]);
+console.log(graph1.depthFirstSearch());
+console.log(graph2.depthFirstSearch());
+console.log(graph3.depthFirstSearch());
+console.log(graph1.breadthFirstSearch());
+console.log(graph2.breadthFirstSearch());
+console.log(graph3.breadthFirstSearch());
 console.log('Для [[1,6,14],[1,2,7],[1,3,9],[2,4,15],[2,3,10],[3,6,2],[3,4,11],[6,5,9],[5,4,6]]');
-console.log((new Graph([[1,6,14],[1,2,7],[1,3,9],[2,4,15],[2,3,10],[3,6,2],[3,4,11],[6,5,9],[5,4,6]])).theShortestDistence(1,2));
-console.log((new Graph([[1,6,14],[1,2,7],[1,3,9],[2,4,15],[2,3,10],[3,6,2],[3,4,11],[6,5,9],[5,4,6]])).theShortestDistence(1,3));
-console.log((new Graph([[1,6,14],[1,2,7],[1,3,9],[2,4,15],[2,3,10],[3,6,2],[3,4,11],[6,5,9],[5,4,6]])).theShortestDistence(1,4));
-console.log((new Graph([[1,6,14],[1,2,7],[1,3,9],[2,4,15],[2,3,10],[3,6,2],[3,4,11],[6,5,9],[5,4,6]])).theShortestDistence(1,5));
-console.log((new Graph([[1,6,14],[1,2,7],[1,3,9],[2,4,15],[2,3,10],[3,6,2],[3,4,11],[6,5,9],[5,4,6]])).theShortestDistence(1,6));
+console.log(graph4.theShortestDistance(1,2));
+console.log(graph4.theShortestDistance(1,3));
+console.log(graph4.theShortestDistance(1,4));
+console.log(graph4.theShortestDistance(1,5));
+console.log(graph4.theShortestDistance(1,6));
 console.log('Для [[0,3,5],[1,3,11],[2,3,56],[4,3,77],[5,4,89]]');
-console.log((new Graph([[0,3,5],[1,3,11],[2,3,56],[4,3,77],[5,4,89]])).theShortestDistence(0,2));
-console.log((new Graph([[0,3,5],[1,3,11],[2,3,56],[4,3,77],[5,4,89]])).theShortestDistence(0,3));
-console.log((new Graph([[0,3,5],[1,3,11],[2,3,56],[4,3,77],[5,4,89]])).theShortestDistence(0,4));
-console.log((new Graph([[0,3,5],[1,3,11],[2,3,56],[4,3,77],[5,4,89]])).theShortestDistence(0,5));
+console.log(graph5.theShortestDistance(0,2));
+console.log(graph5.theShortestDistance(0,3));
+console.log(graph5.theShortestDistance(0,4));
+console.log(graph5.theShortestDistance(0,5));
